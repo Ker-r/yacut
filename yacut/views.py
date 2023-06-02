04a17cb852f0c6834765e198349d0e86f7ceb5de
Каролина Ефремова <efremova.karolina@gmail.com>
2023-06-02 23:25:54 +0700

    v3

--- yacut/views.py
+++ yacut/views.py
@@ -12,26 +12,25 @@ def index_view():
     # Если ошибок не возникло, то
     if not form.validate_on_submit():
         return render_template('yacut.html', form=form)
-    else:
-        short_id = form.custom_id.data
-        # Если в БД уже есть ссылка, которую ввёл пользователь,
-        if URLMap.query.filter_by(short=short_id).first():
-            # вызвать функцию flash и передать соответствующее сообщение
-            flash(f'Имя {short_id} уже занято!')
-            # и вернуть пользователя на главную страницу
-            return render_template('yacut.html', form=form)
-        if short_id is None or short_id == '':
-            short_id = get_unique_short_id()
-        # нужно создать новый экземпляр класса URLMap
-        url_map = URLMap(
-            original=form.original_link.data,
-            short=short_id
-        )
-        # Затем добавить его в сессию работы с базой данных
-        db.session.add(url_map)
-        # И зафиксировать изменения
-        db.session.commit()
-        return render_template('yacut.html', form=form, short=short_id), 200
+    short_id = form.custom_id.data
+    # Если в БД уже есть ссылка, которую ввёл пользователь,
+    if URLMap.query.filter_by(short=short_id).first():
+        # вызвать функцию flash и передать соответствующее сообщение
+        flash(f'Имя {short_id} уже занято!')
+        # и вернуть пользователя на главную страницу
+        return render_template('yacut.html', form=form)
+    if short_id is None or short_id == '':
+        short_id = get_unique_short_id()
+    # нужно создать новый экземпляр класса URLMap
+    url_map = URLMap(
+        original=form.original_link.data,
+        short=short_id
+    )
+    # Затем добавить его в сессию работы с базой данных
+    db.session.add(url_map)
+    # И зафиксировать изменения
+    db.session.commit()
+    return render_template('yacut.html', form=form, short=short_id), 200
 
 
 @app.route('/<string:short>')
